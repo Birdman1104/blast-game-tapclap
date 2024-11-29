c1441ac3d5d4fd371ff68b9c80b9c3904b4e1812
Birdman1104 <arsenmazmanyan11@gmail.com>
2024-11-29 14:54:57 +0400

    Added Game model.

---
+++ src/configs/GameConfig.ts
@@ -0,0 +1,13 @@
+export const GAME_CONFIG = Object.freeze({
+    moves: 25,
+    colors: [
+        //
+        'red',
+        'green',
+        'blue',
+        'yellow',
+        'purple',
+    ],
+    adjacentTilesCount: 2,
+    targetScore: 100,
+});
--- src/models/GameModel.ts
+++ src/models/GameModel.ts
@@ -1,11 +1,18 @@
+import { GAME_CONFIG } from '../configs/GameConfig';
 import { ObservableModel } from './ObservableModel';
 
 export enum GameState {
     Unknown,
+    Playing,
+    Win,
+    Lose,
 }
 
 export class GameModel extends ObservableModel {
     private _state: GameState;
+    private _moves = 0;
+    private _score = 0;
+    private _targetScore = 0;
 
     constructor() {
         super('GameModel');
@@ -22,7 +29,38 @@ export class GameModel extends ObservableModel {
         this._state = value;
     }
 
+    get moves(): number {
+        return this._moves;
+    }
+
+    set moves(value: number) {
+        this._moves = value;
+    }
+
+    get score(): number {
+        return this._score;
+    }
+
+    set score(value: number) {
+        this._score = value;
+    }
+
+    get targetScore(): number {
+        return this._targetScore;
+    }
+
+    set targetScore(value: number) {
+        this._targetScore = value;
+    }
+
+    public setState(state: GameState): void {
+        this._state = state;
+    }
+
     public init(): void {
-        this._state = GameState.Unknown;
+        const { moves, targetScore } = GAME_CONFIG;
+        this._moves = moves;
+        this._targetScore = targetScore;
+        this.setState(GameState.Playing);
     }
 }
